f3fa654c069da752abf0151e0712f86022a4c868
Sam Corin <samcorin@gmail.com>
2017-04-04 11:29:24 +0900

    Time for lunch

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  root to: 'tasks#index'
+
+  # resources :restaurants
+  # resources :restaurants, only: [:create, :index, :destory]
+  # resources :restaurants, except: ['what you dont want']
+
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  post 'tasks', to: 'tasks#create'
+
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  get 'tasks', to: 'tasks#index'
+
+  # edit
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch 'tasks/:id', to: 'tasks#update'
+
+  delete 'tasks/:id', to: 'tasks#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
